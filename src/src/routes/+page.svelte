c14faa4160f86cb78e2c11b096c9d736474c1b1c
Bence Mihucza <mihben@gmail.com>
2026-09-21 15:03:23 +0200

    feat: Add basic layout for overview and services page

--- src/src/routes/+page.svelte
+++ src/src/routes/+page.svelte
@@ -4,54 +4,18 @@
             display: grid;
             grid-template-rows: auto 1fr;
         }
-
-        &.header {
-            display: grid;
-            grid-template-columns: 1fr auto;
-        }
-
-        &.title {
-            display: grid;
-            grid-template-rows: auto auto;
-            grid-gap: var(--spacing--1);
-        }
-
-        &.status {
-            display: grid;
-            grid-template-columns: auto auto;
-            grid-gap: var(--spacing--1);
-
-            align-items: center;
-        }
-    }
-
-    span {
-        &.subtitle {
-            color: var(--foreground--secondary);
-        }
-
-        &.refresh {
-            color: var(--foreground--secondary);
-        }
-    }
-
-    i {
-        width: 24px;
-        height: 24px;
-
-        color: var(--foreground--secondary);
     }
 </style>
 
 <div class="container">
-    <div class="header">
-        <div class="title">
+    <div class="page__header">
+        <div class="page__title">
             <span class="heading__h1">Overview</span>
-            <span class="heading__h4 subtitle">Your services at a glance</span>                    
+            <span class="heading__h4 page__subtitle">Your services at a glance</span>                    
         </div>
-        <div class="status">
-            <i class="ri-refresh-line"></i>
-            <span class="body__small refresh">Updated 2 minutes ago</span>
+        <div class="page__updated">
+            <i class="ri-refresh-line page__subtitle"></i>
+            <span class="body__small page__subtitle">Updated 2 minutes ago</span>
         </div>
     </div>
 </div>
